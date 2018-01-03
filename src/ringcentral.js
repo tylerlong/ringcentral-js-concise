4c9c89d6603a6d7b6960715cf508eb01117061cd
Tyler Long <tyler4long@gmail.com>
2018-01-03 13:34:46 +0800

    First working authorize

--- src/ringcentral.js
+++ src/ringcentral.js
@@ -1,21 +1,22 @@
 const axios = require('axios')
+const { Base64 } = require('js-base64')
+const path = require('path')
+const querystring = require('querystring')
 
 class RingCentral {
   constructor (clientId, clientSecret, server) {
     this.clientId = clientId
     this.clientSecret = clientSecret
-    const userAgent = 'tylerlong/ringcentral-js-concise'
-    this.rc = axios.create({
-      baseURL: server,
-      headers: {
-        'RC-User-Agent': userAgent,
-        'User-Agent': userAgent
-      }
-    })
+    this.server = server
   }
 
   async authorize ({ username, extension, password }) {
-    const r = await this.rc.post('/restapi/oauth/token', { grant_type: 'password', username, extension, password })
+    const r = await axios({
+      method: 'post',
+      url: path.join(this.server, '/restapi/oauth/token'),
+      data: querystring.stringify({ grant_type: 'password', username, extension, password }),
+      headers: { Authorization: `Basic ${Base64.encode(`${this.clientId}:${this.clientSecret}`)}` }
+    })
     return r
   }
 }
