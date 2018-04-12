e860c3fb3322817f77e3c141ec03c2801ed52d1a
Tyler Long <tyler4long@gmail.com>
2018-04-12 22:45:23 +0800

    Add test case for WebHook

---
+++ test/webhook.spec.js
@@ -0,0 +1,43 @@
+/* eslint-env jest */
+const RingCentral = require('../src/ringcentral')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+jest.setTimeout(256000)
+
+const rc = new RingCentral(process.env.clientId, process.env.clientSecret, process.env.server)
+
+function timeout (ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+describe('webhook', () => {
+  test('Glip post notification', async () => {
+    await rc.authorize({
+      username: process.env.username,
+      extension: process.env.extension,
+      password: process.env.password
+    })
+    try {
+      const res = await rc.post('/restapi/v1.0/subscription', {
+        eventFilters: [ '/restapi/v1.0/glip/posts' ],
+        deliveryMode: {
+          transportType: 'WebHook',
+          address: 'https://9047074b.ngrok.io/webhook'
+        }
+      })
+      console.log(res)
+    } catch (e) {
+      console.log(e)
+      console.log(e.response)
+      console.log(e.response.data)
+      return
+    }
+    await timeout(5000)
+    await rc.post(`/restapi/v1.0/glip/groups/${process.env.glipGroupId}/posts`, {
+      text: 'Hello world'
+    })
+    await timeout(200000)
+  })
+})
