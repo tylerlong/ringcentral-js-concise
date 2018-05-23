a2ed916c6b4286cebc576c7861ef259c2793fcda
Tyler Long <tyler4long@gmail.com>
2018-05-23 09:52:10 +0800

    Add sms test case

---
+++ test/sms.spec.js
@@ -0,0 +1,29 @@
+/* eslint-env jest */
+const RingCentral = require('../src/ringcentral')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+jest.setTimeout(64000)
+
+const rc = new RingCentral(process.env.RINGCENTRAL_CLIENT_ID, process.env.RINGCENTRAL_CLIENT_SECRET, process.env.RINGCENTRAL_SERVER_URL)
+
+describe('ringcentral', () => {
+  test('sms', async () => {
+    await rc.authorize({
+      username: process.env.RINGCENTRAL_USERNAME,
+      extension: process.env.RINGCENTRAL_EXTENSION,
+      password: process.env.RINGCENTRAL_PASSWORD
+    })
+
+    const r = await rc.post('/restapi/v1.0/account/~/extension/~/sms', {
+      to: [{ phoneNumber: process.env.RINGCENTRAL_RECEIVER }],
+      from: { phoneNumber: process.env.RINGCENTRAL_USERNAME },
+      text: 'Hello world'
+    })
+
+    expect(r.status).toBe(200)
+
+    await rc.revoke()
+  })
+})
