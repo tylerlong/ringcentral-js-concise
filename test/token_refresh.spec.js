603b334d27a87c2dbceb29b9609916068c092d32
Tyler Long <tyler4long@gmail.com>
2018-06-09 11:23:32 +0800

    Add token refresh spec

---
+++ test/token_refresh.spec.js
@@ -0,0 +1,44 @@
+/* eslint-env jest */
+const RingCentral = require('../src/ringcentral')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+jest.setTimeout(64000)
+
+const rc = new RingCentral(process.env.RINGCENTRAL_CLIENT_ID, process.env.RINGCENTRAL_CLIENT_SECRET, process.env.RINGCENTRAL_SERVER_URL)
+
+describe('ringcentral', () => {
+  test('sms', async () => {
+    await rc.authorize({
+      username: process.env.RINGCENTRAL_USERNAME,
+      extension: process.env.RINGCENTRAL_EXTENSION,
+      password: process.env.RINGCENTRAL_PASSWORD
+    })
+
+    const token = rc.token() // save the token
+    await rc.refresh()
+    rc.token(token) // restore the old token
+
+    try {
+      await rc.post('/restapi/v1.0/account/~/extension/~/sms', {
+        to: [{ phoneNumber: process.env.RINGCENTRAL_RECEIVER }],
+        from: { phoneNumber: process.env.RINGCENTRAL_USERNAME },
+        text: 'Hello world'
+      })
+    } catch (e) {
+      expect(e.response.status).toBe(401)
+    }
+
+    await rc.refresh() // refresh again with old refresh token
+
+    const r2 = await rc.post('/restapi/v1.0/account/~/extension/~/sms', {
+      to: [{ phoneNumber: process.env.RINGCENTRAL_RECEIVER }],
+      from: { phoneNumber: process.env.RINGCENTRAL_USERNAME },
+      text: 'Hello world'
+    })
+    expect(r2.status).toBe(200)
+
+    await rc.revoke()
+  })
+})
