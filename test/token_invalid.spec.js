680efaa62a02f6d5a9be543ed2d74fa3b482b1bc
Tyler Long <tyler4long@gmail.com>
2018-11-27 16:49:54 +0800

    More test cases for invalid token issue

--- test/token_invalid.spec.js
+++ test/token_invalid.spec.js
@@ -42,7 +42,6 @@ describe('token invalid', () => {
     try {
       await rc.refresh()
     } catch (e) {
-      console.log('exception')
       expect(e.status).toBe(400)
       expect(e.data.error_description).toBe('Invalid token')
     } finally {
