8307fadece2c07deb2dec1e7dee37f75a3a35f8c
Tyler Long <tyler4long@gmail.com>
2018-11-28 15:40:23 +0800

    Add test case for call log with recording

---
+++ test/call_log_with_recording.spec.js
@@ -0,0 +1,30 @@
+/* eslint-env jest */
+import RingCentral from '../src/ringcentral'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+jest.setTimeout(64000)
+
+const rc = new RingCentral(process.env.RINGCENTRAL_CLIENT_ID, process.env.RINGCENTRAL_CLIENT_SECRET, process.env.RINGCENTRAL_SERVER_URL)
+
+describe('ringcentral', () => {
+  test('call logs with recording', async () => {
+    await rc.authorize({
+      username: process.env.RINGCENTRAL_USERNAME,
+      extension: process.env.RINGCENTRAL_EXTENSION,
+      password: process.env.RINGCENTRAL_PASSWORD
+    })
+
+    let r = await rc.get('/restapi/v1.0/account/~/extension/~/call-log', { params: {
+      withRecording: true,
+      dateFrom: '2016-03-10T18:07:52.534Z',
+      perPage: 3,
+      view: 'Detailed',
+      type: 'Voice'
+    } })
+    console.log(r.data)
+    console.log(r)
+    await rc.revoke()
+  })
+})
