2f5c817d0f8c928eb65b5c9f5f259d321d724eba
Tyler Long <tyler4long@gmail.com>
2019-02-19 11:01:27 +0800

    Rewrite authorize_uri test

--- test/authorize_uri.spec.js
+++ test/authorize_uri.spec.js
@@ -1,7 +1,5 @@
 /* eslint-env jest */
 import RingCentral from '../src/ringcentral'
-import querystring from 'querystring'
-import URI from 'urijs'
 
 jest.setTimeout(128000)
 
@@ -9,22 +7,13 @@ const rc = new RingCentral(process.env.RINGCENTRAL_CLIENT_ID, process.env.RINGCE
 
 describe('authorze uri', () => {
   test('authorize uri', async () => {
-    const data = querystring.stringify({
+    const r = await rc.get('/restapi/oauth/authorize', { params: {
       response_type: 'code',
       redirect_uri: 'https://ringcentral.github.io/ringcentral-js-widget/page/redirect.html',
       state: 'myState',
       client_id: process.env.RINGCENTRAL_CLIENT_ID
-    })
-
-    const r = await rc._axios.request({
-      method: 'post',
-      url: URI(rc.server).path('/restapi/oauth/authorize').toString(),
-      data,
-      headers: rc._basicAuthorizationHeader()
-    })
+    } })
     expect(r.status).toBe(200)
     expect(r.data).toContain('<!DOCTYPE HTML>') // returned content is an HTML page
-
-    await rc.revoke()
   })
 })
